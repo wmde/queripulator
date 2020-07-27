057344d64e644a89bf93a508300bf4d3438ef290
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-07-27 12:41:38 +0200

    Use QueryHandlerChain in index.js
    Remove the index.js handler chain again and instead use the
    QueryHandlerChain that was added in the meantime, which depends on the
    SPARQL query text, not the whole client request.

--- index.js
+++ index.js
@@ -3,45 +3,42 @@
 const http = require( 'http' );
 const https = require( 'https' );
 
+const QueryHandlerChain = require( './lib/QueryHandlerChain' );
+
 const port = process.argv[ 2 ] || 8080;
 
-const wdqsProxy = {
-	analyse( _clientRequest ) {
-		return true; // this is the last fallback, it always matches
-	},
-	handle( analyseResult, clientRequest, clientResponse ) {
-		const wdqsRequest = https.request(
-			'https://query.wikidata.org',
-			{
-				path: clientRequest.url,
-				method: clientRequest.method,
-				headers: {
-					...clientRequest.headers,
-					host: 'query.wikidata.org',
-				},
-			},
-			function ( wdqsResponse ) {
-				clientResponse.writeHead( wdqsResponse.statusCode, wdqsResponse.headers );
-				wdqsResponse.pipe( clientResponse, { end: true } );
-			},
-		);
-		clientRequest.pipe( wdqsRequest, { end: true } );
-	},
-};
-
-const handlers = [
-	wdqsProxy,
-];
+const queryHandlerChain = new QueryHandlerChain( [] );
 
 http.createServer( function ( clientRequest, clientResponse ) {
-	for ( const handler of handlers ) {
-		const analyseResult = handler.analyse( clientRequest );
-		if ( analyseResult ) {
-			handler.handle( analyseResult, clientRequest, clientResponse );
-			return;
+	const extraResponseHeaders = {};
+	if ( clientRequest.method === 'GET' ) {
+		const url = new URL( clientRequest.url, 'http://localhost' );
+		if ( url.pathname === '/sparql' && url.searchParams.has( 'query' ) ) {
+			const result = queryHandlerChain.getResult( url.searchParams.get( 'query' ) );
+			extraResponseHeaders[ 'X-Simple-Query' ] = result.isSimple() ? 'true' : 'false';
 		}
 	}
-	// this should never happen, the last handler should always match
-	clientResponse.writeHead( 500 );
-	clientResponse.end( 'No suitable handler found.\n' );
+
+	const wdqsRequest = https.request(
+		'https://query.wikidata.org',
+		{
+			path: clientRequest.url,
+			method: clientRequest.method,
+			headers: {
+				...clientRequest.headers,
+				host: 'query.wikidata.org',
+			},
+		},
+		function ( wdqsResponse ) {
+			clientResponse.writeHead(
+				wdqsResponse.statusCode,
+				{
+					...wdqsResponse.headers,
+					...extraResponseHeaders,
+				},
+			);
+			wdqsResponse.pipe( clientResponse, { end: true } );
+		},
+	);
+	clientRequest.pipe( wdqsRequest, { end: true } );
 } ).listen( port );
