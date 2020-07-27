332058323c82af3aae3234aa32ee2bf8332d0d28
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-07-27 12:22:39 +0200

    Potentially chain multiple handlers
    There is a list of handlers, each with an analyse() function and a
    handle() function. If analyse() returns a truthy value, then this
    handler is used (its handle() called, with the result of analyse() to
    facilitate data transfer between both phases), otherwise the next
    handler is tried. Right now only a single handler is registered, a
    fallback which always matches and proxies to the Wikidata Query Service.

--- index.js
+++ index.js
@@ -5,21 +5,43 @@ const https = require( 'https' );
 
 const port = process.argv[ 2 ] || 8080;
 
-http.createServer( function ( clientRequest, clientResponse ) {
-	const wdqsRequest = https.request(
-		'https://query.wikidata.org',
-		{
-			path: clientRequest.url,
-			method: clientRequest.method,
-			headers: {
-				...clientRequest.headers,
-				host: 'query.wikidata.org',
+const wdqsProxy = {
+	analyse( _clientRequest ) {
+		return true; // this is the last fallback, it always matches
+	},
+	handle( analyseResult, clientRequest, clientResponse ) {
+		const wdqsRequest = https.request(
+			'https://query.wikidata.org',
+			{
+				path: clientRequest.url,
+				method: clientRequest.method,
+				headers: {
+					...clientRequest.headers,
+					host: 'query.wikidata.org',
+				},
 			},
-		},
-		function ( wdqsResponse ) {
-			clientResponse.writeHead( wdqsResponse.statusCode, wdqsResponse.headers );
-			wdqsResponse.pipe( clientResponse, { end: true } );
-		},
-	);
-	clientRequest.pipe( wdqsRequest, { end: true } );
+			function ( wdqsResponse ) {
+				clientResponse.writeHead( wdqsResponse.statusCode, wdqsResponse.headers );
+				wdqsResponse.pipe( clientResponse, { end: true } );
+			},
+		);
+		clientRequest.pipe( wdqsRequest, { end: true } );
+	},
+};
+
+const handlers = [
+	wdqsProxy,
+];
+
+http.createServer( function ( clientRequest, clientResponse ) {
+	for ( const handler of handlers ) {
+		const analyseResult = handler.analyse( clientRequest );
+		if ( analyseResult ) {
+			handler.handle( analyseResult, clientRequest, clientResponse );
+			return;
+		}
+	}
+	// this should never happen, the last handler should always match
+	clientResponse.writeHead( 500 );
+	clientResponse.end( 'No suitable handler found.\n' );
 } ).listen( port );
