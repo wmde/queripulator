33304b706219e8a28725494e3968a621fdcfe56a
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-07-29 18:42:09 +0200

    Add TruthyHandler
    This detects when a query uses only truthy triples. The idea is that
    such a query could be sent to a different query service (cluster), which
    holds a smaller amount of data and may be more efficient.
    
    The defaultQueryHandlerChain is reordered overall, because
    SubjectByPropertyValueHandler needs to come before TruthyHandler,
    otherwise it’ll never match (any subject-by-property query is also
    truthy). This probably makes sense in general, too – I’m not sure why
    SubjectByPropertyValueHandler was so far down the chain before.

---
+++ tests/TruthyHandler.spec.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const parser = require( '../lib/sparqlParser' );
+const TruthyHandler = require( '../lib/TruthyHandler' );
+const WdqsRequest = require( '../lib/WdqsRequest' );
+
+describe( 'TruthyHandler', () => {
+
+	const handler = new TruthyHandler();
+
+	it.each( [
+		'SELECT * WHERE { ?item wdt:P31 wd:Q5; wdt:P19 wd:Q146. }',
+		'SELECT * WHERE { ?item wdt:P31 wd:Q5; a wdno:P570. }',
+		'ASK { ?s ?p ?o. }',
+	] )( 'detects query as truthy: %s', ( query ) => {
+		expect( handler.handle( query, parser.parse( query ) ) )
+			.toBeInstanceOf( WdqsRequest );
+	} );
+
+	it.each( [
+		'SELECT * WHERE { ?item p:P31/ps:P31 wd:Q486972. }',
+		'SELECT * WHERE { ?item p:P39 [ ps:P39 wd:Q11696; pq:P2715 wd:Q45578 ]. }',
+		'SELECT * WHERE { { OPTIONAL { ?item p:P31/ps:P31 wd:Q486972. } } UNION {} }',
+		`
+SELECT ?item ?itemLabel WHERE {
+  ?item wdt:P31 wd:Q146.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}
+		`.replace( /\s+/gm, ' ' ).trim(),
+	] )( 'rejects query as not truthy: %s', ( query ) => {
+		expect( handler.handle( query, parser.parse( query ) ) )
+			.toBeFalsy();
+	} );
+
+} );
