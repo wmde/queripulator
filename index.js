f4f71f7fc7ffad095c0e1c3d4e0cab7215267ce7
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-07-29 12:01:29 +0200

    Add rudimentary stats module
    Count how often each request type is used, and make the statistics
    available on the /queripulator-stats route, as well as dumping them to
    stdout on graceful exit (because the Cloud VPS service restarts often,
    so without saving the stats to the journal there won’t be much to
    report). Note that this doesn’t distinguish between WdqsRequest objects
    from SpecificItemPropertyPairQueryHandler, OptimizingHandler, or the
    default fallback handler yet.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 const http = require( 'http' );
 const https = require( 'https' );
+const process = require( 'process' );
 
 const OptimizingHandler = require( './lib/OptimizingHandler' );
 const QueryHandlerChain = require( './lib/QueryHandlerChain' );
 const SpecificItemPropertyPairQueryHandler = require( './lib/SpecificItemPropertyPairQueryHandler' );
+const stats = require( './lib/stats' );
 const SubjectByPropertyValueHandler = require( './lib/SubjectByPropertyValueHandler' );
 
 const port = process.argv[ 2 ] || 8080;
@@ -24,15 +26,23 @@ function isJsonType( mimeType ) {
 	}
 }
 
-http.createServer( function ( clientRequest, clientResponse ) {
+const server = http.createServer( function ( clientRequest, clientResponse ) {
 	const extraResponseHeaders = {};
 	if ( clientRequest.method === 'GET' && isJsonType( clientRequest.headers.accept ) ) {
 		const url = new URL( clientRequest.url, 'http://localhost' );
 		if ( url.pathname === '/sparql' && url.searchParams.has( 'query' ) ) {
 			const result = queryHandlerChain.getResult( url.searchParams.get( 'query' ) );
+			stats.incrementCounter( `handler.result.${result.constructor.name}` );
 			result.respond( clientRequest, clientResponse );
 			return;
 		}
+	}
+
+	if ( clientRequest.url === '/queripulator-stats' ) {
+		clientResponse.writeHead( 200, { 'content-type': 'application/json' } );
+		stats.dump( clientResponse );
+		clientResponse.end();
+		return;
 	}
 
 	const wdqsRequest = https.request(
@@ -57,4 +67,12 @@ http.createServer( function ( clientRequest, clientResponse ) {
 		},
 	);
 	clientRequest.pipe( wdqsRequest, { end: true } );
-} ).listen( port );
+} );
+
+process.on( 'SIGTERM', () => {
+	server.close( () => {
+		stats.dump( process.stdout );
+	} );
+} );
+
+server.listen( port );
