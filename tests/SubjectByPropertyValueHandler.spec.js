20b0fb6f3b3ea4a443c99f182c0b73e88bd965bf
Jakob Warkotsch <j.warkotsch@gmail.com>
2020-07-29 16:34:16 +0200

    SubjectByPropertyValueHandler: fix single triple detection

--- tests/SubjectByPropertyValueHandler.spec.js
+++ tests/SubjectByPropertyValueHandler.spec.js
@@ -12,6 +12,7 @@ describe( 'SubjectByPropertyValueHandler', () => {
 		[ 'SELECT * WHERE { ?s ?p ?o. }' ],
 		[ 'SELECT ?values { wd:Q42 wdt:P31 ?values. }' ],
 		[ 'SELECT ?item (NOW() AS ?asOf) WHERE { ?item wdt:P123 "456" }' ],
+		[ 'SELECT ?urls WHERE { { wd:Q21980377 p:P856 [wikibase:rank wikibase:PreferredRank; ps:P856 ?urls]. } }' ],
 	] )( 'does not handle query: %s', ( query ) => {
 		const handler = new SubjectByPropertyValueHandler();
 		expect( handler.handle( query, parser.parse( query ) ) ).toBeFalsy();
