f3c302749e52db3a4ce570e8cae0ddce782a06c6
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-07-30 18:10:39 +0200

    Don’t use axios for WdqsRequest
    The main motivation for this is to forward all the request headers to
    WDQS, because they’re probably useful. This includes Accept-Encoding,
    which lets the query server respond with compressed data, which can be
    useful; however, that is also why we have to stop using axios: axios
    “transparently” decompresses the response, but does not remove or adjust
    the Content-Length header, so the browser will see a truncated response.
    
    This effectively reverts bf0e2cf75e.

--- tests/WdqsRequest.spec.js
+++ tests/WdqsRequest.spec.js
@@ -1,67 +1,69 @@
 'use strict';
 
-const axios = require( 'axios' );
+const https = require( 'https' );
 const MockReq = require( 'mock-req' );
 const MockRes = require( 'mock-res' );
 const { Readable } = require( 'stream' );
 const WdqsRequest = require( '../lib/WdqsRequest' );
 
-jest.mock( 'axios' );
+jest.mock( 'https' );
 
 describe( 'WdqsRequest', () => {
 
 	beforeEach( jest.clearAllMocks );
 
-	function mockAxiosResponse( response = {} ) {
-		return {
-			status: response.status || 200,
-			headers: response.headers || {},
-			data: response.data || Readable.from( [] ),
-		};
+	function mockResponse( response = {} ) {
+		const mockRes = new MockRes();
+		mockRes.writeHead( response.statusCode || 200, response.headers || {} );
+		( response.data || Readable.from( [] ) ).pipe( mockRes );
+		return mockRes;
 	}
 
-	it( 'URL-encodes the query', async () => {
+	it( 'URL-encodes the query', () => {
 		const request = new WdqsRequest( 'ASK { ?s ?p ?o. }' );
-		axios.get.mockResolvedValue( mockAxiosResponse() );
-		await request.respond( new MockReq(), new MockRes() );
-		expect( axios.get ).toHaveBeenCalledTimes( 1 );
-		expect( axios.get.mock.calls[ 0 ][ 0 ] ).toBe(
+		request.respond( new MockReq(), new MockRes() );
+		expect( https.get ).toHaveBeenCalledTimes( 1 );
+		expect( https.get.mock.calls[ 0 ][ 0 ] ).toBe(
 			'https://query.wikidata.org/sparql?query=ASK%20%7B%20%3Fs%20%3Fp%20%3Fo.%20%7D',
 		);
 	} );
 
 	it( 'extends the user agent', async () => {
 		const request = new WdqsRequest( '' );
-		axios.get.mockResolvedValue( mockAxiosResponse() );
 		request.respond(
 			new MockReq( { headers: { 'user-agent': 'foo bar' } } ),
 			new MockRes(),
 		);
-		expect( axios.get ).toHaveBeenCalledTimes( 1 );
-		expect( axios.get.mock.calls[ 0 ][ 1 ].headers[ 'user-agent' ] ).toBe(
+		expect( https.get ).toHaveBeenCalledTimes( 1 );
+		expect( https.get.mock.calls[ 0 ][ 1 ].headers[ 'user-agent' ] ).toBe(
 			'foo bar Queripulator/1.0.0',
 		);
 	} );
 
 	it( 'streams response data', async () => {
 		const request = new WdqsRequest( '' );
-		axios.get.mockResolvedValue( mockAxiosResponse( {
-			data: Readable.from( [ 'data1', 'data2' ] ),
-		} ) );
+		https.get.mockImplementation( ( _url, _options, callback ) => {
+			callback( mockResponse( {
+				data: Readable.from( [ 'data1', 'data2' ] ),
+			} ) );
+		} );
 		const response = new MockRes();
-		await request.respond( new MockReq(), response );
+		request.respond( new MockReq(), response );
 		await new Promise( ( resolve ) => response.on( 'finish', resolve ) );
 		// eslint-disable-next-line no-underscore-dangle
 		expect( response._getString() ).toBe( 'data1data2' );
 	} );
 
-	it( 'closes response on axios error', async () => {
+	it( 'forwards non-2xx status code', async () => {
 		const request = new WdqsRequest( '' );
-		axios.get.mockRejectedValue();
+		https.get.mockImplementation( ( _url, _options, callback ) => {
+			callback( mockResponse( {
+				statusCode: 500,
+			} ) );
+		} );
 		const response = new MockRes();
-		jest.spyOn( response, 'end' );
-		await request.respond( new MockReq(), response );
-		expect( response.end ).toHaveBeenCalledTimes( 1 );
+		request.respond( new MockReq(), response );
+		await new Promise( ( resolve ) => response.on( 'finish', resolve ) );
 		expect( response.statusCode ).toBe( 500 );
 	} );
 
