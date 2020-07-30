cacd928c63d8c999ec7375338ca0790fbfa1901d
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-07-30 14:43:55 +0200

    Handle simple BGP queries using LDF
    Using the deprecated ldf-client library [1], because the suggested
    replacement Comunica has a bug which prevents it from communicating with
    the Wikidata LDF service [2]. ldf-client also turns out to have a bug –
    if the query uses DISTINCT, LIMIT, or OFFSET, the results lose their
    head – but that one is easier to work around.
    
    [1]: https://github.com/LinkedDataFragments/Client.js
    [2]: https://github.com/comunica/comunica/issues/700

---
+++ tests/LdfHandler.spec.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const parser = require( '../lib/sparqlParser' );
+const LdfHandler = require( '../lib/LdfHandler' );
+const LdfRequest = require( '../lib/LdfRequest' );
+
+describe( 'LdfHandler', () => {
+
+	const handler = new LdfHandler();
+
+	it.each( [
+		'SELECT * WHERE { ?s ?p ?o. }',
+		'SELECT * WHERE { ?item wdt:P31 wd:Q5; wdt:P19 wd:Q4007. }',
+	] )( 'handles basic query: %s', ( query ) => {
+		expect( handler.handle( query, parser.parse( query ) ) )
+			.toBeInstanceOf( LdfRequest );
+	} );
+
+	it.each( [
+		'SELECT * WHERE { BIND(wd:Q5 AS ?class) ?item wdt:P31 ?class. }',
+		`
+SELECT ?item ?itemLabel WHERE {
+  ?item wdt:P31 wd:Q146.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}
+		`.replace( /\s+/gm, ' ' ).trim(),
+	] )( 'does not handle unsupported query: %s', ( query ) => {
+		expect( handler.handle( query, parser.parse( query ) ) )
+			.toBeFalsy();
+	} );
+
+	it.each( [
+		'SELECT DISTINCT ?item WHERE { ?item wd:P1087 []. }',
+		'SELECT ?item WHERE { ?item wdt:P31 wd:Q5. } LIMIT 10',
+		'SELECT ?item WHERE { ?item wdt:P31 wd:Q146. } OFFSET 0',
+	] )( 'does not handle query that would trigger ldf-client bug: %s', ( query ) => {
+		expect( handler.handle( query, parser.parse( query ) ) )
+			.toBeFalsy();
+	} );
+
+} );
