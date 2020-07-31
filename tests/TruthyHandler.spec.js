39f27e0a2760a370024cf7fe53092ae618e107ac
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-07-31 12:33:30 +0200

    Allow label service in truthy handler
    I had assumed the truthy dumps did not contain labels, descriptions, or
    aliases, and therefore the truthy service would not be able to support
    the label service. But in fact it looks like those terms are all
    included, so as far as I understand there’s little reason to exclude the
    label service.

--- tests/TruthyHandler.spec.js
+++ tests/TruthyHandler.spec.js
@@ -11,6 +11,12 @@ describe( 'TruthyHandler', () => {
 	it.each( [
 		'SELECT * WHERE { ?item wdt:P31 wd:Q5; wdt:P19 wd:Q146. }',
 		'SELECT * WHERE { ?item wdt:P31 wd:Q5; a wdno:P570. }',
+		`
+SELECT ?item ?itemLabel WHERE {
+  ?item wdt:P31 wd:Q146.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}
+		`.replace( /\s+/gm, ' ' ).trim(),
 	] )( 'detects query as truthy: %s', ( query ) => {
 		expect( handler.handle( query, parser.parse( query ) ) )
 			.toBeInstanceOf( TruthyWdqsRequest );
@@ -22,12 +28,6 @@ describe( 'TruthyHandler', () => {
 		'SELECT (COUNT(*) AS ?c) WHERE { ?property wikibase:claim ?p. ?item ?p ?statement. }',
 		'SELECT * WHERE { ?item ?p [ ?ps wd:Q109411 ]. }',
 		'SELECT * WHERE { { OPTIONAL { ?item p:P31/ps:P31 wd:Q486972. } } UNION {} }',
-		`
-SELECT ?item ?itemLabel WHERE {
-  ?item wdt:P31 wd:Q146.
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-}
-		`.replace( /\s+/gm, ' ' ).trim(),
 	] )( 'rejects query as not truthy: %s', ( query ) => {
 		expect( handler.handle( query, parser.parse( query ) ) )
 			.toBeFalsy();
